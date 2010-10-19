b39b0e4d2dede6b9a9209b6afc08d39f4a25954a
Abhishek Shukla <abhishekpshukla@gmail.com>
2010-10-20 00:51:41 +0530

    removed birthdate validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -39,7 +39,7 @@ class User < ActiveRecord::Base
   validates_presence_of     :metro_area, :if => Proc.new { |user| user.state }
   validates_uniqueness_of   :login_slug
   validates_exclusion_of    :login, :in => AppConfig.reserved_logins
-  validates_date :birthday, :before => 13.years.ago.to_date  
+  #validates_date :birthday, :before => 13.years.ago.to_date
 
   #associations
     has_enumerated :role  
